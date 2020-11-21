b26be82fa9577c4ef0860c73c27aafb06dbcc4d7
pavankpdev <pavankumarkpkp@gmail.com>
2020-11-21 18:20:05 +0530

    nginx routing fix 2

--- server/node_server/database/connectDB.js
+++ server/node_server/database/connectDB.js
@@ -16,7 +16,7 @@ const mysqlConnection = mysql.createConnection({
   host:
     process.env.NODE_ENV === "development" ? "localhost" : process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PWD,
+  password: process.env.NODE_ENV === "development" ? "pavan" : process.env.DB_PWD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   timeout: 60000,
